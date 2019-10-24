3f8192ea042ad11db7d9133e5eba71cb4939ee53
Konstantin <konzi.h@gmail.com>
2019-10-24 17:21:14 +0200

    Minigame basics. Outline of Whiteboard Game

---
+++ GameDesign_Unity/Assets/Scripts/Minigame/Minigame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Minigame : MonoBehaviour
+{
+    public MinigameInput Input { get; set; }
+
+    public Transform[] APositions;
+
+    public Transform[] BPositions;
+
+    protected void Finish()
+    {
+        //Input.GameManager.FinishGame(this);
+    }
+}
---
+++ GameDesign_Unity/Assets/Scripts/Minigame/MinigameInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameInput
+{
+    public float TimeScale { get; private set; }
+    public float ADifficulty { get; private set; }
+    public float BDifficulty { get; private set; }
+    public object GameManager { get; private set; }
+
+    /// <summary>
+    /// Holds all input information for a Minigame.
+    /// </summary>
+    /// <param name="timeScale">Affects the duration a player has to complete the game, range [-1,1].</param
+    /// <param name="aDifficulty">Affects the difficulty to 'catch' the first answer, range [-1,1].</param>
+    /// <param name="bDifficulty">Affects the difficulty to 'catch' the second answer, range [-1,1].</param>
+    /// <param name="gameManager">Reference to the GameManager which spawned the minigame.</param>
+    public MinigameInput(float timeScale, float aDifficulty, float bDifficulty, object gameManager)
+    {
+        TimeScale = timeScale;
+        ADifficulty = aDifficulty;
+        BDifficulty = bDifficulty;
+        GameManager = gameManager;
+    }
+}
