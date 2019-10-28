a1d4f29cc8458451769fb3bd74a1f8fde987abe6
Horst <e08330864@student.tuwien.ac.at>
2019-10-28 12:10:20 +0100

    Conversation Panel

---
+++ GameDesign_Unity/Assets/Scripts/Model/Answer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Answer : MonoBehaviour
+{
+    private string text = "";
+    private float startValuePatience = 0f;
+    private float stopValuePatience = 0f;
+    private float startValueEnergy = 0f;
+    private float stopValueEnergy = 0f;
+
+    public Answer(
+        string text,
+        float startValuePatience,
+        float stopValuePatience,
+        float startValueEnergy,
+        float stopValueEnergy)
+    {
+        this.text = text;
+        this.startValuePatience = startValuePatience;
+        this.stopValuePatience = stopValuePatience;
+        this.startValueEnergy = startValueEnergy;
+        this.stopValueEnergy = stopValueEnergy;
+    }
+
+
+}
---
+++ GameDesign_Unity/Assets/Scripts/Model/Question.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+    private string text = "";
+    private List<Answer> answers = new List<Answer>();
+    // Start is called before the first frame updateb
+    void Start()
+    {
+        
+    }
+
+    //-------------------------------------------------------------
+    public void AddAnswer(
+        string text,
+        float startValuePatience,
+        float stopValuePatience,
+        float startValueEnergy,
+        float stopValueEnergy)
+    {
+        answers.Add(new Answer(text, startValuePatience, stopValuePatience, startValueEnergy, stopValueEnergy));
+    }
+}
