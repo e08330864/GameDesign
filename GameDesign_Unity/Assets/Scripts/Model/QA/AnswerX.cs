126021d1e8ab7715f4d76bfa10562b8ae747a563
Horst <e08330864@student.tuwien.ac.at>
2019-10-29 08:45:59 +0100

    Story Model v01

--- GameDesign_Unity/Assets/Scripts/Model/QA/AnswerX.cs
+++ GameDesign_Unity/Assets/Scripts/Model/QA/AnswerX.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnswerX : MonoBehaviour
+public class AnswerX
 {
+    // parameters
     private string text = "";
     private float startValuePatience = 0f;
     private float stopValuePatience = 0f;
     private float startValueEnergy = 0f;
     private float stopValueEnergy = 0f;
+    // sied effects of given answere
+    private AnswerXdictSideEffect sideEffects = new AnswerXdictSideEffect();
 
     public AnswerX (
         string text,
@@ -22,5 +25,15 @@ public class AnswerX : MonoBehaviour
         this.stopValuePatience = stopValuePatience;
         this.startValueEnergy = startValueEnergy;
         this.stopValueEnergy = stopValueEnergy;
+    }
+
+    public void AddSideEffect(AnswerX effectedAnswer, float effectMulFac)
+    {
+        sideEffects.Add(effectedAnswer, effectMulFac);
+    }
+
+    public AnswerXdictSideEffect GetSideEffects()
+    {
+        return sideEffects;
     }
 }
