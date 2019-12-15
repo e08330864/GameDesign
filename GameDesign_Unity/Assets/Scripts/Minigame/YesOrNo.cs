3586cec1878c4d535c378c21cc8fc166c4ea083c
XIII07 <leanderlol@gmx.at>
2019-12-15 04:47:10 +0100

    skippen und 1 neue Szene

---
+++ GameDesign_Unity/Assets/Scripts/Minigame/YesOrNo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YesOrNo : MinigameController
+{
+    public GameObject yesObject;
+    public GameObject noObject;
+
+
+
+    public void AnswerYes()
+    {
+      
+        var yes = new Answer(AnswerValue.YES, yesTimelineText, yesDeltas);
+
+        FinishLevel(yes);
+
+    }
+    public void AnswerNo()
+    {
+        StopAllCoroutines();
+        var no = new Answer(AnswerValue.NO, noTimelineText, noDeltas);
+
+        FinishLevel(no);
+
+    }
+
+}
