ff22651065213f0977f93eee1c822b9b0f320b46
Horst <e08330864@student.tuwien.ac.at>
2020-01-16 19:48:18 +0100

    WT targets

---
+++ GameDesign_Unity/Assets/Scripts/Minigame/WilhelmTell/Target.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField]
+    private Ring ringPrefab = null;
+    private List<Ring> rings = new List<Ring>();
+    private WilhelmTell wilhelmTell = null;
+
+    private int level = -1;
+    private Vector2 startPosition;
+    private Vector2 stopPosition;
+    private Vector3 velocity;
+    private Sprite bullEyeSprite;
+    private Sprite secondRingSprite;
+    private int targetSize;
+    private int numberOfTargetRings;
+    private float targetSpeed;
+
+    private bool isInstanziated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isInstanziated)
+        {
+            if (level != -1)
+            {
+                wilhelmTell = gameObject.transform.parent.parent.GetComponent<WilhelmTell>();
+                transform.localPosition = new Vector3(startPosition.x, startPosition.y, 0);
+                for (int i = numberOfTargetRings; i > 0; i--)
+                {
+                    Ring ring = Instantiate(ringPrefab);
+                    Image img = ring.gameObject.GetComponent<Image>();
+                    img.sprite = (i % 2 == 1) ? bullEyeSprite : secondRingSprite;
+                    ring.gameObject.transform.SetParent(gameObject.transform, false);
+                    ring.gameObject.transform.localPosition = Vector3.zero;
+                    RectTransform rt = (RectTransform)ring.transform;
+                    float scale = targetSize / (numberOfTargetRings * 2 - 1) * (i * 2 - 1) / rt.rect.width;
+                    Debug.Log("rect-width=" + img.sprite.rect.width);
+                    Debug.Log("scale=" + scale);
+                    ring.gameObject.transform.localScale = new Vector3(scale, scale, 1.0f);
+                }
+                isInstanziated = true;
+            }
+        }
+        else
+        {
+            // movement of target
+
+        }
+    }
+
+    public void SetParameters(int level,
+                              Vector2 startPosition,
+                              Vector2 stopPosition,
+                              Sprite bullEyeSprite,
+                              Sprite secondRingSprite,
+                              int targetSize = 100, 
+                              int numberOfTargetRings = 4, 
+                              float targetSpeed = 1.0f)
+    {
+        this.level = level;
+        this.startPosition = startPosition;
+        this.stopPosition = stopPosition;
+        this.velocity = (stopPosition - startPosition).normalized * targetSpeed;
+        this.bullEyeSprite = bullEyeSprite;
+        this.secondRingSprite = secondRingSprite;
+        this.targetSize = targetSize;
+        this.numberOfTargetRings = numberOfTargetRings;
+        this.targetSpeed = targetSpeed;
+    }
+}
---
+++ GameDesign_Unity/Assets/Scripts/Minigame/WilhelmTell/WilhelmTell.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WilhelmTell : MonoBehaviour
+{
+    [SerializeField]
+    private int level = 1;
+    [SerializeField]
+    private Target targetPrefab = null;
+    [SerializeField]
+    private Sprite bullEyeSprite = null;
+    [SerializeField]
+    private Sprite secondRingSprite = null;
+    [SerializeField]
+    private int numberOfTargets = 1;
+    [SerializeField]
+    private int targetSize = 800;
+    [SerializeField]
+    private int numberOfTargetRings = 3;
+    [SerializeField]
+    private float targetSpeed = 3.0f;
+
+    private List<Target> targets = new List<Target>();
+    private Vector3 sizeInScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sizeInScreen = transform.Find("Range").GetComponent<SpriteRenderer>().bounds.size;
+        for (int i = 0; i < numberOfTargets; i++)
+        {
+            Target target = Instantiate(targetPrefab);
+            target.transform.SetParent(this.transform, false);
+            target.SetParameters(level, 
+                new Vector2(200, 200), 
+                new Vector2(200, 200), 
+                bullEyeSprite, 
+                secondRingSprite, 
+                targetSize, 
+                numberOfTargetRings, 
+                targetSpeed);
+            targets.Add(target);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
