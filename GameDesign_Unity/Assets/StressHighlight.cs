b072ceefc04a313c5b4e38368a9344d8893ffba2
Konstantin <konzi.h@gmail.com>
2020-01-25 14:56:47 +0100

    Highlight Stress if nearly full

---
+++ GameDesign_Unity/Assets/StressHighlight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StressHighlight : MonoBehaviour
+{
+    public Color stressedColor;
+
+    Color defaultColor;
+    Image bg;
+    Stress playerStress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStress = FindObjectOfType<Stress>();
+        bg = GetComponent<Image>();    
+        defaultColor = bg.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerStress.Value >= 4)
+        {
+            bg.color = Color.Lerp(defaultColor, stressedColor, Mathf.PingPong(Time.time, 1));
+        }
+        else
+        {
+            bg.color = defaultColor;
+        }
+    }
+}
